23307486c98adf86a2933ee4a7c603a770ab8a73
Jordan Lund <jlund@mozilla.com>
2016-04-06 18:26:58 -0700

    condition off whether or not this_chunk is one of the big_chunks or not

--- chunkify/__init__.py
+++ chunkify/__init__.py
@@ -13,13 +13,11 @@ def chunkify(things, this_chunk, chunks):
     big_chunks = len(things) % chunks
     start = size * (this_chunk-1)
     if big_chunks:
-        start += (min(big_chunks, this_chunk)-1)
-        if this_chunk <= big_chunks:
+        if this_chunk <= big_chunks:  # this_chunk is a big chunk
             size += 1
-        elif this_chunk == big_chunks+1:
-            start += 1
+            start = size * (this_chunk-1)
         else:
-            start += this_chunk-big_chunks-1
+            start += big_chunks  # account for the extra 1 'thing' in each big_chunk
 
     end = start + size
     try:
